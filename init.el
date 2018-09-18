1644941c5414bcd371f291ca5a9c00eab3da807b
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-09-18 10:24:14 +0800

    un-display frame-title * format as default

--- init.el
+++ init.el
@@ -454,8 +454,8 @@ It should only modify the values of Spacemacs settings."
    ;; %z - mnemonics of buffer, terminal, and keyboard coding systems
    ;; %Z - like %z, but including the end-of-line format
    ;; (default "%I@%S")
-   ;; dotspacemacs-frame-title-format "%I@%S"
-   dotspacemacs-frame-title-format "%a @ %m"
+   dotspacemacs-frame-title-format "%I@%S"
+   ;; dotspacemacs-frame-title-format "%a @ %m"
 
    ;; Format specification for setting the icon title format
    ;; (default nil - same as frame-title-format)
