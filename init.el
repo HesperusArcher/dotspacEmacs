ac8f8b57480ee25cb72c1a9d49a07a3bca64d506
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-09-29 09:29:53 +0800

    * .emacs.d/init (setq melpa-cn) may not work

--- init.el
+++ init.el
@@ -503,10 +503,10 @@ configuration.
 It is mostly for variables that should be set before packages are loaded.
 If you are unsure, try setting them in `dotspacemacs/user-config' first."
 
-  ;; (setq configuration-layer-elpa-archives
-  ;;       '(("melpa-cn" . "https://elpa.zilongshanren.com/melpa/")
-  ;;         ("org-cn"   . "https://elpa.zilongshanren.com/org/")
-  ;;         ("gnu-cn"   . "https://elpa.zilongshanren.com/gnu/")))
+  (setq configuration-layer-elpa-archives
+        '(("melpa-cn" . "https://elpa.zilongshanren.com/melpa/")
+          ("org-cn"   . "https://elpa.zilongshanren.com/org/")
+          ("gnu-cn"   . "https://elpa.zilongshanren.com/gnu/")))
 
   ;; ;; https://github.com/syl20bnr/spacemacs/issues/2705
   ;; ;; (setq tramp-mode nil)
