c9896f813576cd6c94413a661b3197b79a325409
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-11-08 22:05:18 +0800

    - mv hook to .emacs.d/xxxx-mode.el

--- layers/hesperus/config.el
+++ layers/hesperus/config.el
@@ -21,52 +21,6 @@
 (setq imenu-list-position 'left)
 
 
-;;; --- add-hook ---
-
-;; improve prog-mode with rainbow-delimiters
-(add-hook 'prog-mode-hook
-          (lambda ()
-            (rainbow-delimiters-mode)
-            ))
-
-;; improve the performance of opening tex file
-(add-hook 'LaTeX-mode-hook
-          (lambda ()
-            (setq TeX-engine 'xetex
-                  TeX-show-compilation t)
-            (auto-fill-mode -1)
-            (setq truncate-lines nil)  ;; truncate lines ignore words
-            (set (make-local-variable 'pangu-spacing-real-insert-separtor) t)
-            ))
-
-;; improve the performance of opening markdown file
-(add-hook 'markdown-mode-hook
-          (lambda ()
-            ;; (auto-fill-mode)  ;; if column ==80 return
-            ;; (setq truncate-lines nil)  ;; truncate lines ignore words
-            ;; (visual-line-mode t)
-            (set (make-local-variable 'pangu-spacing-real-insert-separtor) t)
-            ;; (spacemacs/toggle-centered-point-on)
-            ))
-
-;; improve the performance of opening org file
-;; (add-hook 'org-mode-hook (lambda () (spacemacs/toggle-line-numbers-off)) 'append)
-(add-hook 'org-mode-hook
-          (lambda ()
-            ;; (auto-fill-mode)  ;; if column ==80 return
-            (setq truncate-lines nil)  ;; truncate lines ignore words
-            ;; (visual-line-mode t)
-            (set (make-local-variable 'pangu-spacing-real-insert-separtor) t)
-            ;; (spacemacs/toggle-centered-point-on)
-            ))
-
-;; improve the performance of opening python file
-(add-hook 'python-mode-hook
-          (lambda ()
-            ;; set anaconda's ipython as default python interpreter
-            (setq python-shell-interpreter "/opt/anaconda/bin/ipython")
-            ))
-
 ;; ;; line-numbers for yaml-mode
 ;; (add-hook 'yaml-mode-hook (lambda () (spacemacs/toggle-line-numbers-on)) 'append)
 
